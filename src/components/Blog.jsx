20b4ed99d1f864ed4b4eaf9e8f233d274dbec502
mstflotfy <mstf.lotfy@gmail.com>
2024-06-04 23:50:43 +0300

    submit: 5.7 Blog List Frontend, step 7

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,37 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import Togglable from "./Togglable"
+
+const Blog = ({ blog }) => {
+
+  const toggleLike = () => {
+    console.log('likes + 1')
+  }
+
+  const listItemStyle = {
+    display: 'flex',
+    gap: '1.2rem',
+  }
+
+  return (
+    <div className="Blog">
+      {blog.title} 
+      <Togglable
+        showButtonText="view"
+      >
+        <div className="Blog__details">
+          <p>
+            URL: {blog.url}
+          </p>
+          <p style={listItemStyle}>
+            Likes: {blog.likes}
+            <button onClick={toggleLike}>Likes</button>
+          </p>
+          <p>
+            Author: {blog.author}
+          </p>
+        </div>
+      </Togglable>
+    </div>  
+  )
+}
 
 export default Blog
