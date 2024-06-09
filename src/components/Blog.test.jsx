dd0e37642692920e976f9a26099681b529107969
mstflotfy <mstf.lotfy@gmail.com>
2024-06-09 09:09:40 +0300

    submit: 5.15: Blog List Tests, step 3

--- src/components/Blog.test.jsx
+++ src/components/Blog.test.jsx
@@ -1,16 +1,18 @@
 import { render, screen } from '@testing-library/react'
-import { beforeEach, describe, expect, test } from 'vitest'
+import { beforeEach, describe, expect, test, vi } from 'vitest'
 import Blog from './Blog'
 import userEvent from '@testing-library/user-event'
 
 /*
-5.14: Blog List Tests, step 2
+5.15: Blog List Tests, step 3
 
-Make a test, which checks that the blog's URL and number of likes are shown when the button controlling the shown details has been clicked.
+Make a test, which ensures that if the like button is clicked twice, the event handler the component received as props is called twice.
 */
 
 describe('<Blog />', () => {
   let container
+  const actingUser = userEvent.setup()
+  let showButton
 
   const blog = {
     title: 'test blog title',
@@ -26,8 +28,14 @@ describe('<Blog />', () => {
     username: 'mstflotfy'
   }
 
+  const handleLike = vi.fn()
+  const handleDel = vi.fn()
+
   beforeEach(() => {
-    container = render(<Blog blog={blog} user={user} />).container
+    container = render(
+      <Blog blog={blog} user={user} handleIncrementLikes={handleLike} handleDeletePost={handleDel} />
+    ).container
+    showButton = screen.getByText('view')
   })
 
   test('Rednders blog\'s title & author, and does not render url or likes, by default', () => {
@@ -38,12 +46,21 @@ describe('<Blog />', () => {
   })
 
   test('url & likes appear when view button is clicked', async () => {
-    const user = userEvent.setup()
-    const showButton = screen.getByText('view')
 
-    await user.click(showButton)
+    await actingUser.click(showButton)
 
     expect(container).toHaveTextContent(blog.url)
     expect(container).toHaveTextContent(blog.likes)
+  })
+
+  test('when like is pressed twice the handleLike event is called twice', async () => {
+    await actingUser.click(showButton)
+    const likeButton = screen.getByText('Like')
+
+    await actingUser.dblClick(likeButton)
+
+    expect(handleLike.mock.calls).toHaveLength(2)
+    // or
+    expect(handleLike).toHaveBeenCalledTimes(2)
   })
 })
